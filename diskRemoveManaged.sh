26ed0d2f2f03cc84dd048d1d0abbe7fc53ce78a5
Oliver Young <oliver.young@digital.justice.gov.uk>
2019-04-17 15:21:43 +0100

    Added disk removal scripts

---
+++ diskRemoveManaged.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Script to remove all unused managed disks in your default subscription
+
+deleteUnattachedDisks=1
+
+unattachedDiskIds=$(az disk list --query '[?managedBy==`null`].[id]' -o tsv)
+for id in ${unattachedDiskIds[@]}
+do
+    if (( $deleteUnattachedDisks == 1 ))
+    then
+
+        echo "Deleting unattached Managed Disk with Id: "$id
+        az disk delete --ids $id --yes
+        echo "Deleted unattached Managed Disk with Id: "$id
+
+    else
+        echo $id
+    fi
+done
---
+++ diskRemoveUnmanaged.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# Script that deletes all un-managed disks. This script iterates over all storage accounts in your default subscription
+# Set deleteUnattachedVHDs=1 if you want to delete unattached VHDs
+# Set deleteUnattachedVHDs=0 if you want to see the details of the unattached VHDs
+deleteUnattachedVHDs=0
+
+storageAccountIds=$(az storage account list --query [].[id] -o tsv)
+
+for id in ${storageAccountIds[@]}
+do
+    connectionString=$(az storage account show-connection-string --ids $id --query connectionString -o tsv)
+    containers=$(az storage container list --connection-string $connectionString --query [].[name] -o tsv)
+    for container in ${containers[@]}
+    do
+        blobs=$(az storage blob list -c $container --connection-string $connectionString --query "[?properties.blobType=='PageBlob' && ends_with(name,'.vhd')].[name]" -o tsv)
+        for blob in ${blobs[@]}
+        do
+            leaseStatus=$(az storage blob show -n $blob -c $container --connection-string $connectionString --query "properties.lease.status" -o tsv)
+            if [ "$leaseStatus" == "unlocked" ]
+            then
+                if (( $deleteUnattachedVHDs == 1 ))
+                then
+                    echo "Deleting VHD: "$blob" in container: "$container" in storage account: "$id
+                    az storage blob delete --delete-snapshots include  -n $blob -c $container --connection-string $connectionString
+                    echo "Deleted VHD: "$blob" in container: "$container" in storage account: "$id
+                else
+                    echo "StorageAccountId: "$id" container: "$container" VHD: "$blob
+                fi
+            fi
+        done
+    done
+done
