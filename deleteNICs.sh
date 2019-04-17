ee16b4eafabb4d53c748ec34408a9e684a1c8635
Oliver Young <oliver.young@digital.justice.gov.uk>
2019-04-17 15:14:41 +0100

    Added delete nics script

---
+++ deleteNICs.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Script for deleting unused NICs. This does not iterate over subscriptions
+# You need to: az account set --subscription $subscription first. 
+# Set deleteUnattachedNics=1 if you want to delete unattached NICs
+# Set deleteUnattachedNics=0 if you want to see the Id(s) of the unattached NICs
+deleteUnattachedNics=1
+
+unattachedNicsIds=$(az network nic list --query '[?virtualMachine==`null`].[id]' -o tsv)
+for id in ${unattachedNicsIds[@]}
+do
+   if (( $deleteUnattachedNics == 1 ))
+   then
+
+       echo "Deleting unattached NIC with Id: "$id
+       az network nic delete --ids $id
+       echo "Deleted unattached NIC with Id: "$id
+   else
+       echo $id
+   fi
+done
